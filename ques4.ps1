e86f5cc42ab3b94d67bd384da233227fe0a33fe2
Iamneo <techadmin@iamneo.ai>
2023-09-16 04:06:53 +0000

    Backup Commit

--- ques4.ps1
+++ ques4.ps1
@@ -1,5 +1,8 @@
 $n1 = read-host "Enter first number: "
 $n2 = read-host "Enter second number: "
-if($n1 + $n2 -gt 70){
-    write-host 
+if(($n1 + $n2) -gt 70){
+    write-host "Pass"
+}
+else {
+    write-host "Fail"
 }
