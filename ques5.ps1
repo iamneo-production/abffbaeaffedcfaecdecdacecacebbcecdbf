f2fa45ad719215012be643eab17984cd09755d68
Iamneo <techadmin@iamneo.ai>
2023-09-16 04:26:53 +0000

    Backup Commit

---
+++ ques5.ps1
@@ -0,0 +1,18 @@
+#first part
+new-item Testing -itemType directory
+#second part
+cd Testing
+new-item SubFolder1 -itemType directory
+new-item SubFolder2 -itemType directory
+#third part
+cd SubFolder1
+new-item Test1.text
+new-item Test2.txt
+new-item Test3.txt
+#fourth part
+cd SubFolder2
+new-item Test4.text
+new-item Test5.txt
+new-item Test6.txt
+#writing a statement.
+write-host "All the folders and files are created as mentioned."
