5816ee906c3c54e89f233b57cbc33891ee113e33
Iamneo <techadmin@iamneo.ai>
2023-09-16 04:01:53 +0000

    Backup Commit

---
+++ ques3.ps1
@@ -0,0 +1,4 @@
+$dd = read-host "Enter date(DD): "
+$mm = read-host "Enter month(MM): "
+$yy = read-host "Enter year(YY): "
+write-host "Date of birth(DOB) is : $dd-$mm-$yy"
---
+++ ques4.ps1
@@ -0,0 +1,5 @@
+$n1 = read-host "Enter first number: "
+$n2 = read-host "Enter second number: "
+if($n1 + $n2 -gt 70){
+    write-host 
+}
